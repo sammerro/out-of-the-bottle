5540e1704fb54d768659954281dda47a3eeabcc3
sammerro <michal.kowalski.wro@gmail.com>
2026-03-08 20:22:39 +0100

    add some script for identity

--- wine-blog/app/app.vue
+++ wine-blog/app/app.vue
@@ -8,3 +8,23 @@
     <Footer />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+  ]
+})
+
+onMounted(() => {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on('init', user => {
+      if (!user) {
+        window.netlifyIdentity.on('login', () => {
+          document.location.href = '/admin/'
+        })
+      }
+    })
+  }
+})
+</script>
